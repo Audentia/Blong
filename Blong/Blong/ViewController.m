0cdbfd26c88937e0ac4d2bb4eeb6019630a91ab2
Douglas Hewitt <doughewitt4@gmail.com>
2015-07-20 20:04:50 -0700

    Pan Gesture Working
    Pan gesture moves paddle and does not take it outside the view.
    Disabled rotation so it works in any orientation
    
    down to 128 lines :D

--- Blong/Blong/ViewController.m
+++ Blong/Blong/ViewController.m
@@ -14,12 +14,6 @@
 @property (nonatomic, strong) UIPushBehavior *pusher;
 @property (nonatomic, strong) UICollisionBehavior *collider;
 
-@property (nonatomic, strong) UISnapBehavior *snap;
-@property (nonatomic, strong) UIView *snapArea;
-
-@property (nonatomic, assign) CGRect snapRect;
-@property (nonatomic, assign) CGPoint snapPoint;
-
 @property (nonatomic, strong) UIDynamicItemBehavior *ballDynamicProperties;
 @property (nonatomic, strong) UIDynamicItemBehavior *paddleDynamicProperties;
 
@@ -27,10 +21,6 @@
 @property (nonatomic, strong) UIView *ballView;
 
 @property (readwrite, assign) float dx;
-@property (readwrite, assign) float dy;
-
-@property (nonatomic, strong) UIPanGestureRecognizer *panGesture;
-
 
 @end
 
@@ -38,19 +28,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    basic setup for any game
     self.view.backgroundColor = [UIColor blackColor];
-
-    [self addSnapArea];
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     
-    self.dx = 0;
-    self.dy = 0;
-
-    
+//    methods to start specific games
     [self startGame];
-    
-
 }
 
 - (void)startGame {
@@ -58,13 +41,11 @@
     [self createPlayerPaddle];
 //    [self createAIPaddle];
     [self createCollisions];
-
 }
 
 - (void)createCollisions {
-    // Add collisions
     self.collider = [[UICollisionBehavior alloc] initWithItems:@[self.ballView, self.paddleView]];
-//    self.collider.collisionDelegate     = self.paddleView;
+//    self.collider.collisionDelegate = self.paddleView;
     [self.animator addBehavior:self.collider];
     self.collider.collisionMode = UICollisionBehaviorModeEverything;
     self.collider.translatesReferenceBoundsIntoBoundary = YES;
@@ -76,28 +57,27 @@
     self.ballView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.ballView];
     
-    // Remove rotation
+//    Remove rotation
     self.ballDynamicProperties = [[UIDynamicItemBehavior alloc] initWithItems:@[self.ballView]];
     self.ballDynamicProperties.allowsRotation = NO;
     [self.animator addBehavior:self.ballDynamicProperties];
     
-    // Better Bounce
+//    Proper bounce that won't gain energy
     self.ballDynamicProperties.elasticity = 1.0;
     self.ballDynamicProperties.friction = 0.0;
     self.ballDynamicProperties.resistance = 0.0;
     
+//    Start the ball
     self.pusher = [[UIPushBehavior alloc] initWithItems:@[self.ballView]
                                                    mode:UIPushBehaviorModeInstantaneous];
-    
     self.pusher.pushDirection = CGVectorMake(0.1, 0.1);
     self.pusher.active = YES;
-    // Because push is instantaneous, it will only happen once
+//    Because push is instantaneous, it will only happen once
     [self.animator addBehavior:self.pusher];
-
 }
 
 - (void)createPlayerPaddle {
-    CGRect paddleRect = CGRectMake((self.view.frame.size.width / 2), (self.view.frame.size.height - 200), 100, 10);
+    CGRect paddleRect = CGRectMake((self.view.frame.size.width / 2), (self.view.frame.size.height - 30), 100, 10);
     self.paddleView = [[UIView alloc] initWithFrame:paddleRect];
     self.paddleView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.paddleView];
@@ -110,66 +90,39 @@
     //make heavy
     self.paddleDynamicProperties.density = 1000.0f;
     
-
-    
+    //allow user to move
+    self.dx = 0;
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(movePaddle:)];
+    [panGesture setMinimumNumberOfTouches:1];
+    [panGesture setMaximumNumberOfTouches:1];
+    [self.view addGestureRecognizer:panGesture];
 }
-- (IBAction)didPanPlayerPaddle:(UIPanGestureRecognizer *)sender {
-    self.panGesture = sender;
-    [self.paddleView addGestureRecognizer:self.panGesture];
 
+- (void)movePaddle:(UIPanGestureRecognizer *)sender {
     if ((sender.state == UIGestureRecognizerStateBegan || sender.state ==UIGestureRecognizerStateChanged) && (sender.numberOfTouches == 1)) {
         
-        //Remove any previous snap to avoid flicker if both gesture and physics are on the view at same time
-        if (self.snap != nil) {
-            [self.animator removeBehavior:self.snap];
-        }
         CGPoint location = [sender locationInView:self.view];
         
         if (self.dx == 0) {
             self.dx = location.x - self.paddleView.center.x;
         }
-        if (self.dy == 0) {
-            self.dy = location.y - self.paddleView.center.y;
-        }
         
-        //apply offsets
-        CGPoint newLocation = CGPointMake(location.x - self.dx, location.y - self.dy);
-        self.paddleView.center = newLocation;
+        //create offsets
+        CGPoint newLocation = CGPointMake(location.x - self.dx, self.paddleView.center.y);
+        
+        //keep paddle inside view
+        if (CGRectContainsPoint (self.view.frame, newLocation)) {
+            //apply offsets
+            self.paddleView.center = newLocation;
+        }
+        //update animations
+        [self.animator updateItemUsingCurrentState:self.paddleView];
+
 
     } else if (sender.state == UIGestureRecognizerStateEnded) {
         //reset offsets when dragging ends so they will be recalculated correctly
         self.dx = 0;
-        self.dy = 0;
-        
-        [self snapImageIntoPlace:[sender locationInView:self.view]];
     }
-}
-
-- (void)addSnapArea {
-    self.snapRect = CGRectMake(self.view.center.x - 100, self.view.center.y - 100, 200, 200);
-    self.snapPoint = CGPointMake(self.snapRect.size.width / 2, self.snapRect.size.height / 2);
-    
-    self.snapArea = [[UIView alloc] initWithFrame:self.snapRect];
-    self.snapArea.backgroundColor = [UIColor whiteColor];
-    [self.view addSubview:self.snapArea];
-}
-
-- (void)snapImageIntoPlace:(CGPoint)touchPoint {
-    if (CGRectContainsPoint(self.snapRect, touchPoint)) {
-        if (self.snap != nil) {
-            [self.animator removeBehavior:self.snap];
-        }
-        self.snap = [[UISnapBehavior alloc] initWithItem:self.paddleView snapToPoint:self.snapPoint];
-        [self.animator addBehavior:self.snap];
-    }
-}
-
-
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
